7a4026c3ffe67236393ee756fc9298baba549fd5
PraneethJain <praneethjain005@gmail.com>
2022-06-04 11:19:13 +0530

    Finished functionality of calculator

--- main.js
+++ main.js
@@ -11,44 +11,60 @@ function operate(operator, a, b) {
     }
 }
 
-let displayValue = '';
+
 let currentValue = 0;
-let operatorSelected = false;
 let currentOperator = null;
+let input = ''
+let input2 = ''
+let num1 = null;
+let num2 = null;
 const output = document.querySelector('.output');
 
-const Buttons = document.querySelectorAll('.btn');
-Buttons.forEach(button => {
-    button.addEventListener('click', e=> {
-        if (!operatorSelected && !e.target.classList.contains('operator')) {
-            displayValue = e.target.id;
-            currentValue = parseInt(displayValue);
-        } else if (operatorSelected && !e.target.classList.contains('operator')){
-            displayValue += e.target.id;
-            currentValue = operate(currentOperator, currentValue, parseInt(e.target.id))
-            operatorSelected = false;
-            currentOperator = null;
-        } else if (!operatorSelected && e.target.classList.contains('operator')) {
-            displayValue = currentValue.toString();
-            operatorSelected = true;
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        if (e.target.classList.contains('operator')) {
+            if (num1 === null) {
+                num1 = parseInt(input);
+            } else if (num2===null && input2!=='') {
+                num2 = parseInt(input2);
+                num1 = operate(currentOperator, num1, num2);
+                input2 = '';
+                input = num1.toString()
+                num2 = null;
+            }
             currentOperator = e.target.id;
-            displayValue += e.target.id;
+            input += currentOperator;
         }
-        output.textContent = displayValue;
+        if (!e.target.classList.contains('operator')) {
+            output.textContent = num1;
+            input += e.target.id;
+            if (num1 !== null) {
+                input2 += e.target.id;
+            }
+        }
+        output.textContent = input;
+        console.log(num1);
     })
 })
 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', e=> {
-    displayValue = '0';
-    output.textContent = displayValue;
     currentValue = 0;
-    operatorSelected = false;
     currentOperator = null;
+    input = '0'
+    input2 = ''
+    num1 = null;
+    num2 = null;
+    output.textContent = input;
 })
 
 const equalsButton = document.querySelector('#equals');
 equalsButton.addEventListener('click', e=> {
-    displayValue = currentValue.toString();
-    output.textContent = displayValue;
+    num2 = parseInt(input2);
+    num1 = operate(currentOperator, num1, num2);
+    input2 = '';
+    num2 = null;
+    input = num1.toString();
+    output.textContent = input;
 })
