f4becbb426a89d790a75f1718000ebe58b2af9c5
PraneethJain <praneethjain005@gmail.com>
2022-06-04 10:24:26 +0530

    Implemented functioning calculator

--- main.js
+++ main.js
@@ -17,19 +17,43 @@ function operate(operator, a, b) {
 }
 
 let displayValue = '';
-const output = document.querySelector('.output')
+let currentValue = 0;
+let operatorSelected = false;
+let currentOperator = null;
+const output = document.querySelector('.output');
 
 const Buttons = document.querySelectorAll('.btn');
 Buttons.forEach(button => {
     button.addEventListener('click', e=> {
-        if (displayValue === '0') displayValue = '';
-        displayValue += e.target.id;
+        if (!operatorSelected && !e.target.classList.contains('operator')) {
+            displayValue = e.target.id;
+            currentValue = parseInt(displayValue);
+        } else if (operatorSelected && !e.target.classList.contains('operator')){
+            displayValue += e.target.id;
+            currentValue = operate(currentOperator, currentValue, parseInt(e.target.id))
+            operatorSelected = false;
+            currentOperator = null;
+        } else if (!operatorSelected && e.target.classList.contains('operator')) {
+            displayValue = currentValue.toString();
+            operatorSelected = true;
+            currentOperator = e.target.id;
+            displayValue += e.target.id;
+        }
         output.textContent = displayValue;
     })
 })
 
-const clearButton = document.querySelector('#clear')
+const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', e=> {
     displayValue = '0';
+    output.textContent = displayValue;
+    currentValue = 0;
+    operatorSelected = false;
+    currentOperator = null;
+})
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', e=> {
+    displayValue = currentValue.toString();
     output.textContent = displayValue;
 })
