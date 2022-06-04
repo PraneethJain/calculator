e174bb25be9f1ca6dfb0784aafea51c9a89cd6ff
PraneethJain <praneethjain005@gmail.com>
2022-06-04 10:25:55 +0530

    Simplified operate function

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-const add = (a,b) => a+b;
-const subtract = (a, b) => a-b;
-const multiply = (a, b) => a*b;
-const divide = (a, b) => a/b;
-
 function operate(operator, a, b) {
     switch (operator) {
         case '+':
-            return add(a,b);
+            return a+b;
         case '-':
-            return subtract(a,b);
+            return a-b;
         case '*':
-            return multiply(a,b);
+            return a*b;
         case '/':
-            return divide(a,b);
+            return a/b;
     }
 }
 
